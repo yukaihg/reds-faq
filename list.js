7663ae4c3ceab2a3c3d65e8aa787f8c480c3b8a6
yukaihg <yukaihg@gmail.com>
2012-06-27 13:05:53 -0700

    removed more flies

--- list.js
+++
@@ -1,9 +0,0 @@
-var listOfUrl = [];
-
-listOfUrl.push("http://google.com");
-listOfUrl.push("http://learnboost.com");
-listOfUrl.push("http://ign.com");
-listOfUrl.push("http://expressjs.com");
-listOfUrl.push("http://mongoosejs.com");
-
-module.exports = listOfUrl;
